3c4285e68660de83a0e3975a11df6e2e9b1b3cb9
Ronas Olsen <rono@itu.dk>
2024-11-22 20:46:25 +0100

    Basic monolithic AuctionService in place.

--- server/server.go
+++ server/server.go
@@ -8,8 +8,6 @@ import (
 
 	proto "example.com/auction/grpc"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 /*
@@ -28,6 +26,7 @@ type AuctionService struct {
 	top_bid  *proto.Bid
 	bid_time int64      // Lamport timestamp that increases with each top bid.
 	bid_lock sync.Mutex // Controls access to bid.
+	bidders  int64
 
 	name         string
 	asking_price int64
@@ -46,6 +45,7 @@ func newAuctionService() *AuctionService {
 			BidderId: 0,
 		},
 		bid_time: 0,
+		bidders:  0,
 
 		name:         "Course Book",
 		asking_price: 80,
@@ -82,7 +82,11 @@ func (auction *AuctionService) startService(listener net.Listener) {
 	}
 }
 
-// Get the status of the auction. Returns acknowledgement showing the current auction state.
+/*
+Get the status of the auction. Returns acknowledgement showing the current auction state.
+
+The returned BidderId shows who currently holds the top bid.
+*/
 func (auction *AuctionService) GetAuctionStatus(ctx context.Context, msg *proto.Empty) (*proto.Ack, error) {
 	lead := auction.top_bid
 	answer := &proto.Ack{
@@ -113,9 +117,23 @@ func (auction *AuctionService) GetLot(ctx context.Context, msg *proto.Empty) (*p
 	return answer, nil
 }
 
-// Forward a bid to the auction. Returns acknowledgement showing outcome of the bid.
+/*
+Forward a bid to the auction. Returns acknowledgement showing outcome of the bid.
+
+The returned BidderId allows the service to assign IDs to clients.
+*/
 func (auction *AuctionService) PutBid(ctx context.Context, msg *proto.Bid) (*proto.Ack, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method PutBid not implemented")
+	if msg.BidderId == 0 {
+		auction.bidders++
+		msg.BidderId = auction.bidders
+	}
+	result := auction.processBid(msg)
+	answer := &proto.Ack{
+		Amount:   auction.top_bid.Amount,
+		Result:   result,
+		BidderId: msg.BidderId,
+	}
+	return answer, nil
 }
 
 /*
@@ -123,20 +141,20 @@ Process an incoming bid against the current state of the auction.
 
 Returns 'true' if incoming bid is now the highest and 'false' otherwise.
 */
-func (auction *AuctionService) processBid(incoming *proto.Bid) bool {
+func (auction *AuctionService) processBid(incoming *proto.Bid) proto.StatusValue {
 	if incoming.Amount <= auction.top_bid.Amount ||
 		incoming.Amount < auction.starting_bid { // Pre-lock check. Under-bids cannot obtain lock.
-		return false
+		return proto.StatusValue_UNDERBID
 	}
 
 	auction.bid_lock.Lock()
 	defer auction.bid_lock.Unlock()
 
 	if incoming.Amount <= auction.top_bid.Amount { // Second check once lock is obtained.
-		return false
+		return proto.StatusValue_UNDERBID
 	} else {
 		auction.top_bid = incoming
 		auction.bid_time++
-		return true
+		return proto.StatusValue_ACCEPTED
 	}
 }
